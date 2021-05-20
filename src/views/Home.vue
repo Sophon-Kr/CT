5e2d4e4ac8a1dd1071c90903fbb9a42b96f91b7f
Sophon-Kr <77550438+Sophon-Kr@users.noreply.github.com>
2021-05-21 00:49:46 +0700

    Edit homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
 
 <div class="container"  style="max-width: 1000px;">
-      <div class="" style="min-height: auto;  min-width: auto;">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <div class="col-12 mb-3" style="min-height: auto;  min-width: auto;">
+        <div>
+          
+          <h3 class="col-12"> New Post<img width="40" height="40" src="https://img.icons8.com/cute-clipart/64/000000/pencil.png"/> </h3>
+
+          
+        </div>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+
+  <div class="d-grid  d-md-flex justify-content-md-end" style="margin-top:10px; ">
+<input class="form-control col-2" type="file" id="formFileMultiple" multiple>
+<button type="button" class="btn btn-danger" style="margin-left:10px; ">Danger</button>
+<button type="button" class="btn btn-success" style="margin-left:10px; ">Success</button>
+
+
+</div>
+
 </div>
 </div>
     <div class="container " style="max-width: 1000px;">
@@ -51,7 +65,7 @@
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                           </svg>
                         Danger</button>
-                        <button class="btn btn-outline-warning" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        <button class="btn btn-outline-warning" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-fill" viewBox="0 0 16 16">
                                 <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z"/>
                               </svg>
